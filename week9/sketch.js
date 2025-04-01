60350f9c2b6dba67b1ca20f60ff5cdf673d0d851
DylanF55 <dylanfullerton83@gmail.com>
2025-03-31 22:00:53 -0600

    portraitp5js

---
+++ week9/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(120,440, 220);
+    text('Dylan Fullerton', 10, 10);
+    circle(40,90,70);
+    circle(360,90,70);
+    point(360,90,70);
+    point(40,90,70);
+    square(150,250,90);
+    line(70,20,300,20);
+    triangle(200, 200, 200, 100, 190, 100)
+
+  }
+  
