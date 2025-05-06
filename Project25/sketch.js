a7eff3a936ad4e6414e5b4737340ba8b7b9dd8be
DylanF55 <dylanfullerton83@gmail.com>
2025-05-05 23:12:25 -0600

    scoringandaudiofixed

--- Project25/sketch.js
+++ Project25/sketch.js
@@ -17,6 +17,7 @@ var xNote = 600;
 var yNote = 600;
 var wNote = 200;
 var hNote = 200;
+var noteTime =0;
 var notescore = 0;
 var noteOpen =true; 
 
@@ -33,11 +34,10 @@ music = loadSound("squarehammer.mp3");
 function setup(){
     createCanvas(2150,1080);
     userStartAudio();
-    let playButton = createButton("Begin Game/Test Audio");
+    let playButton = createButton("Test Audio");
     playButton.position(1000, 20);
     playButton.mousePressed(() => {
-        music.loop();
-      });
+        music.loop(); });
 
 }
 
@@ -58,7 +58,34 @@ function draw(){
     fill(25, 200, 255);
     
     image(nb,xNote,yNote, wNote,hNote );
-    fill(25, 200, 255);
+    if (
+      x < xNote + wNote &&
+      x + imW > xNote &&
+      y < yNote + hNote &&
+      y + imWD > yNote
+  ) {
+     
+      xNote = random(0, width - wNote);
+      yNote = random(0, height - hNote); 
+      notescore++;
+      console.log("note block collected Score: " + notescore);
+
+       if (!music.isPlaying()) {
+            music.play();
+        }
+
+        noteTime = millis();
+
+    }
+    if (millis() - noteTime > 10000 && music.isPlaying()) {
+    music.stop();
+    }
+  fill(0,255,0);
+  textFont('Courier New');
+  textSize(40);
+  text("Notes Scored " + notescore,60,60);
+  
+  
 }
 
 function keyPressed(){
@@ -71,4 +98,5 @@ function keyPressed(){
     } else if (keyCode === 40) {
       y += 35;
     }
+    
 }
